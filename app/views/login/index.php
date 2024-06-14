4ed11ce5fa5d7ace20bb32caa285ae2614093cdf
amersohail <amersohail7@gmail.com>
2024-06-14 02:28:45 +0000

    implemented functionality into Registration Controller

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -24,6 +24,7 @@
 		    <button type="submit" class="btn btn-primary">Login</button>
 		</fieldset>
 		</form> 
+			 <p><a href="create/index">Cick here to register for new account</a</p>
 	</div>
 </div>
     <?php require_once 'app/views/templates/footer.php' ?>
