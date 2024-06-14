0239971b7e7dadd8ca8e6d38c27c4a2ddf3e9158
amersohail <amersohail7@gmail.com>
2024-06-14 20:17:29 +0000

    added custom css and referenced it in header templates

--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -14,7 +14,7 @@ if (isset($_SESSION['auth']) == 1) {
         <title>COSC 4806</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
       </head>
-
+  <link href="/app/styles/style.css" rel="stylesheet">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container">
           <a class="navbar-brand" href="#">COSC 4806</a>
